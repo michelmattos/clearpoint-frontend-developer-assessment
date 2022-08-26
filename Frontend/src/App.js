4f3ce030d4e2793365ea3198b79bd07e79694ea4
Michel Mattos <7696411+michelmattos@users.noreply.github.com>
2022-08-26 16:34:25 +1200

    Extracted AddTodoItem and TodoItems components

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
-import { Image, Alert, Button, Container, Row, Col, Form, Table, Stack } from 'react-bootstrap'
+import { Image, Alert, Container, Row, Col } from 'react-bootstrap'
 import React, { useState, useEffect } from 'react'
+import {AddTodoItem} from "./components/AddTodoItem";
+import {TodoItems} from "./components/TodoItems";
 
 const axios = require('axios')
 
@@ -11,73 +13,6 @@ const App = () => {
   useEffect(() => {
     // todo
   }, [])
-
-  const renderAddTodoItemContent = () => {
-    return (
-      <Container>
-        <h1>Add Item</h1>
-        <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
-          <Form.Label column sm="2">
-            Description
-          </Form.Label>
-          <Col md="6">
-            <Form.Control
-              type="text"
-              placeholder="Enter description..."
-              value={description}
-              onChange={handleDescriptionChange}
-            />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
-          <Stack direction="horizontal" gap={2}>
-            <Button variant="primary" onClick={() => handleAdd()}>
-              Add Item
-            </Button>
-            <Button variant="secondary" onClick={() => handleClear()}>
-              Clear
-            </Button>
-          </Stack>
-        </Form.Group>
-      </Container>
-    )
-  }
-
-  const renderTodoItemsContent = () => {
-    return (
-      <>
-        <h1>
-          Showing {items.length} Item(s){' '}
-          <Button variant="primary" className="pull-right" onClick={() => getItems()}>
-            Refresh
-          </Button>
-        </h1>
-
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Description</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.description}</td>
-                <td>
-                  <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
-                    Mark as completed
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </>
-    )
-  }
 
   const handleDescriptionChange = (event) => {
     // todo
@@ -143,11 +78,15 @@ const App = () => {
           </Col>
         </Row>
         <Row>
-          <Col>{renderAddTodoItemContent()}</Col>
+          <Col>
+            <AddTodoItem description={description} handleDescriptionChange={handleDescriptionChange} handleAdd={handleAdd} handleClear={handleClear} />
+          </Col>
         </Row>
         <br />
         <Row>
-          <Col>{renderTodoItemsContent()}</Col>
+          <Col>
+            <TodoItems items={items} getItems={getItems} handleMarkAsComplete={handleMarkAsComplete} />
+          </Col>
         </Row>
       </Container>
       <footer className="page-footer font-small teal pt-4">
---
+++ Frontend/src/components/TodoItems.js
@@ -0,0 +1,36 @@
+import {Button, Table} from "react-bootstrap";
+import React from "react";
+
+export const TodoItems = ({items, getItems, handleMarkAsComplete}) => (
+  <>
+    <h1>
+      Showing {items.length} Item(s){' '}
+      <Button variant="primary" className="pull-right" onClick={() => getItems()}>
+        Refresh
+      </Button>
+    </h1>
+
+    <Table striped bordered hover>
+      <thead>
+      <tr>
+        <th>Id</th>
+        <th>Description</th>
+        <th>Action</th>
+      </tr>
+      </thead>
+      <tbody>
+      {items.map((item) => (
+        <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.description}</td>
+          <td>
+            <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
+              Mark as completed
+            </Button>
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </Table>
+  </>
+)
