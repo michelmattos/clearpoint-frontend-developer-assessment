96d3916a3ebb584dcdab1dff0d79f16f6d729774
Michel Mattos <7696411+michelmattos@users.noreply.github.com>
2022-08-27 14:43:33 +1200

    Added cypress for e2e tests

---
+++ E2E/cypress/e2e/app.cy.js
@@ -0,0 +1,9 @@
+describe('to-do app', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/")
+  })
+
+  it("displays headings", () => {
+    cy.findAllByRole('heading').contains(/add item/i)
+  })
+});
